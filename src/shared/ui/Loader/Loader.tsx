fde9bebe0564ef20accb3577510b055b63516a3d
a.chekhotskiy <a.chekhotskiy@platform.live>
2023-07-18 20:44:54 +0200

    add loki for screenshot regression testing

--- src/shared/ui/Loader/Loader.tsx
+++ src/shared/ui/Loader/Loader.tsx
@@ -4,7 +4,7 @@ import cls from './Loader.module.scss';
 export const Loader: React.FC = () => {
   return (
     <div className={classNames(cls.loader, {}, [])}>
-      <div>LORERERERERER</div>
+      <div></div>
       <div></div>
       <div></div>
       <div></div>
