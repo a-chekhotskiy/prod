f8c174279a570719621dac16cb9dd27ce2b3c41c
a.chekhotskiy <a.chekhotskiy@platform.live>
2023-07-28 13:51:57 +0200

    test error fix

--- src/widgets/Sidebar/ui/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar.test.tsx
@@ -1,15 +1,16 @@
 import { fireEvent, screen } from '@testing-library/react';
 import { renderWithTranslation } from 'helpers/tests/RenderWithTranslation';
+import { componentRender } from 'helpers/tests/componentRender';
 import { Sidebar } from 'widgets/Sidebar';
 
 describe('sidebar tests', () => {
     test('sidebar simpliest test', () => {
-        renderWithTranslation(<Sidebar data-testid="sidebar" />);
+        componentRender(<Sidebar data-testid="sidebar" />);
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     });
 
     test('sidebar opened test', () => {
-        renderWithTranslation(<Sidebar data-testid="sidebar" />);
+        componentRender(<Sidebar data-testid="sidebar" />);
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
         const toggleBtn = screen.getByTestId('toggleButton');
         fireEvent.click(toggleBtn);
